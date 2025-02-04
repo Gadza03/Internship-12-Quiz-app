9645afbe43f147dedfd1731f3574e8b3440b8f59
Gadza03 <mgadza@pmfst.hr>
2025-02-04 13:17:12 +0100

    Start with displaying

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { getQuestions } from "./api.js";
 
 const form = document.querySelector("#quiz-form");
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", async function (event) {
   event.preventDefault();
 
   const category = document.querySelector("#category").value;
@@ -16,5 +16,33 @@ form.addEventListener("submit", function (event) {
   if (type) url.searchParams.append("type", type);
 
   console.log(url.toString());
-  getQuestions(url.toString());
+  try {
+    const questions = await getQuestions(url.toString());
+    displayQuestion(questions);
+  } catch (error) {
+    console.error("Error fetching questions:", error);
+  }
 });
+
+function displayQuestion(questions) {
+  const question = questions[0].question;
+  const answers = [
+    ...questions[0].incorrect_answers,
+    questions[0].correct_answer,
+  ];
+
+  answers = shuffleArray(answers);
+
+  console.log(question);
+
+  const questionElement = document.querySelector(".question");
+  questionElement.innerHTML = question;
+
+  for (let i = 0; i < answers.length; i++) {
+    const answer = answers[i];
+  }
+}
+
+function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
